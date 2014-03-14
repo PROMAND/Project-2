337af1c905a5b941497bbcca7eaf3286589556b6
mihailruskevich <mihailruskevich@inbox.lv>
2014-03-14 03:53:48 +0200

    Track and Talk commit

---
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/domain/Talk.java
@@ -0,0 +1,13 @@
+package pl.byd.wsg.promand.project1.domain;
+
+import java.util.Date;
+
+public class Talk {
+
+    private long id;
+    private String title;
+    private String description;
+    private Date startTime;
+    private Date endTime;
+    //TODO: end/start time, tracks and speaker
+}
