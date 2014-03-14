f3abe95951e566848ade5a2e1a01938e6bde616f
mihailruskevich <mihailruskevich@inbox.lv>
2014-03-14 03:46:03 +0200

    Speaker and Company commit

---
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/domain/Company.java
@@ -0,0 +1,23 @@
+package pl.byd.wsg.promand.project1.domain;
+
+public class Company {
+
+    private String name;
+    private String logoUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+}
---
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/domain/Speaker.java
@@ -0,0 +1,60 @@
+package pl.byd.wsg.promand.project1.domain;
+
+public class Speaker {
+
+    private long id;
+    private String name;
+    private String surname;
+    private String photoUrl;
+    private Company company;
+    private String description;
+    //TODO: profiles
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
