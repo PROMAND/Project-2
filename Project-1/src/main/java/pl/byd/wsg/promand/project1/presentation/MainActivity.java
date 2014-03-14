305481457f010e2314983f8e2fa0d8f844cda47a
Frexas <ruslanas.kudriavcevas@gmail.com>
2014-03-14 19:39:35 +0100

    Tabs are created

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/presentation/MainActivity.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/presentation/MainActivity.java
@@ -1,30 +1,64 @@
 package pl.byd.wsg.promand.project1.presentation;
 
+import android.app.LocalActivityManager;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TabHost;
 
 import pl.byd.wsg.promand.project1.R;
 
 public class MainActivity extends ActionBarActivity {
+    LocalActivityManager mLocalActivityManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLocalActivityManager = new LocalActivityManager(this, false);
+
+        TabHost myTabHost = (TabHost) findViewById(R.id.tabHost);
+        mLocalActivityManager.dispatchCreate(savedInstanceState);
+        myTabHost.setup(mLocalActivityManager);
+
+        TabHost.TabSpec tabSpec = myTabHost.newTabSpec("MyScheduleTab");
+        tabSpec.setIndicator("My\nSchedule", null);
+        Intent myIntent = new Intent(this, MyScheduleTab.class);
+        tabSpec.setContent(myIntent);
+        myTabHost.addTab(tabSpec);
+
+        tabSpec = myTabHost.newTabSpec("allSchedule");
+        tabSpec.setIndicator("All\nSchedule");
+        myIntent = new Intent(this, AllScheduleTab.class);
+        tabSpec.setContent(myIntent);
+        myTabHost.addTab(tabSpec);
+
+        tabSpec = myTabHost.newTabSpec("speakers");
+        tabSpec.setIndicator("Speakers");
+        myIntent = new Intent(this, SpeakersTab.class);
+        tabSpec.setContent(myIntent);
+        myTabHost.addTab(tabSpec);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocalActivityManager.dispatchResume();
+        }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocalActivityManager.dispatchPause(isFinishing());
+        }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
