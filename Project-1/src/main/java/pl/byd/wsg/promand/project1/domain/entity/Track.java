24718bda832c2022eb0658f2760d13727b2ddf1b
mihailruskevich <mihailruskevich@inbox.lv>
2014-03-14 16:43:30 +0200

    Track commit

---
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/domain/entity/Track.java
@@ -0,0 +1,28 @@
+package pl.byd.wsg.promand.project1.domain.entity;
+
+public class Track {
+
+    private long id;
+    private String title;
+
+    public Track(long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
