7f64ecff33b0a1506e516597f84153df57805a97
mihailruskevich <mihailruskevich@inbox.lv>
2014-03-15 22:32:43 +0200

    Talk commit

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/domain/entity/Talk.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/domain/entity/Talk.java
@@ -2,9 +2,6 @@ package pl.byd.wsg.promand.project1.domain.entity;
 
 import java.util.Date;
 
-import pl.byd.wsg.promand.project1.domain.list.SpeakerList;
-import pl.byd.wsg.promand.project1.domain.list.TrackList;
-
 public class Talk {
 
     private long id;
