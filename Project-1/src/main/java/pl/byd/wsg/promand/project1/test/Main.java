87db1d521b68677c1802db889f06d8da757bca5e
mihailruskevich <mihailruskevich@inbox.lv>
2014-03-15 17:24:40 +0200

    main

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/test/Main.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/test/Main.java
@@ -1,20 +1,9 @@
 package pl.byd.wsg.promand.project1.test;
 
-import java.util.List;
-
-import pl.byd.wsg.promand.project1.domain.entity.Speaker;
-import pl.byd.wsg.promand.project1.jsonmock.SpeakerJsonService;
-import pl.byd.wsg.promand.project1.jsonmock.SpeakerJsonServiceImpl;
-import pl.byd.wsg.promand.project1.jsonservice.SpeakerService;
-import pl.byd.wsg.promand.project1.jsonservice.SpeakerServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        SpeakerService speakerService = new SpeakerServiceImpl();
-        List<Speaker> speakerList = speakerService.getSpeakerList();
-        for(Speaker speaker : speakerList){
-            System.out.println(speaker);
-        }
+
     }
 }
