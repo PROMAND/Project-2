2cbbd03b3ef0a2e83fefc788fc19414e3e6431c1
mihailruskevich <mihailruskevich@inbox.lv>
2014-03-16 23:32:09 +0200

    DateUtils  method implemented ( dd.mm.yy format)

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/utils/DateUtils.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/utils/DateUtils.java
@@ -2,12 +2,23 @@ package pl.byd.wsg.promand.project1.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateUtils {
 
     private static final SimpleDateFormat HOUR_MINUTES_FORMAT = new SimpleDateFormat("hh:mm");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yy");
 
     public static String hoursMinutesFormat(Date date) {
         return HOUR_MINUTES_FORMAT.format(date);
     }
+
+    public static String dateFormat(Date date) {
+        return DATE_FORMAT.format(date);
+    }
+
+    public static Date createDate(int year, int month, int day, int hour, int minute) {
+        return new GregorianCalendar(year, month - 1, day, hour, minute).getTime();
+    }
+
 }
