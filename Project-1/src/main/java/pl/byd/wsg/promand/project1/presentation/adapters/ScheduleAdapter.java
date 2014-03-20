401cbcb8f5af481956f44c49f2139f3810415880
rGalrito <Rafaelgalrito@gmail.com>
2014-03-20 21:34:01 +0100

    ScheduleAdapter changes; Real sentence in asadassadsdas

--- Project-1/src/main/java/pl/byd/wsg/promand/project1/presentation/adapters/ScheduleAdapter.java
+++ Project-1/src/main/java/pl/byd/wsg/promand/project1/presentation/adapters/ScheduleAdapter.java
@@ -1,25 +1,26 @@
 package pl.byd.wsg.promand.project1.presentation.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
 import pl.byd.wsg.promand.project1.R;
 import pl.byd.wsg.promand.project1.domain.entity.Talk;
+import pl.byd.wsg.promand.project1.presentation.TalkDescriptionActivity;
 import pl.byd.wsg.promand.project1.utils.DateUtils;
 
 public class ScheduleAdapter extends BaseListAdapter<Talk> {
-
+    Context ct;
     public ScheduleAdapter(Context context, List<Talk> list) {
         super(context, list);
+        this.ct = context;
     }
 
     @Override
@@ -45,8 +46,19 @@ public class ScheduleAdapter extends BaseListAdapter<Talk> {
         Button button = (Button) view.findViewById(R.id.attending_button);
         setupButtonForTalk(button, talk, index);
 
+        Button button1 = (Button) view.findViewById(R.id.btnGoToTalkDesc);
+               button1.setOnClickListener(new View.OnClickListener() {
+                       @Override
+                       public void onClick(View view) {
+                              Intent intent = new Intent (ct, TalkDescriptionActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-//        ImageView imageView = (ImageView) view.findViewById(R.id.talk_image);
+                                       ct.startActivity(intent);
+                           }
+                   });
+                return view;
+            }
+
+/*        ImageView imageView = (ImageView) view.findViewById(R.id.talk_image);
 
         TextView speakerFullName = (TextView) view.findViewById(R.id.speaker_full_name);
         speakerFullName.setText("Tom cruse"); //TODO
@@ -55,7 +67,7 @@ public class ScheduleAdapter extends BaseListAdapter<Talk> {
         trackTitle.setText("100"); //TODO
         return view;
     }
-
+*/
     private void setupButtonForTalk (final Button button, Talk talk, final int index) {
         //TODO: Check if i'm attending to this talk and then mark button
         button.setOnClickListener(new View.OnClickListener() {
